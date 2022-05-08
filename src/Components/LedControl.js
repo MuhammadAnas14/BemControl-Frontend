660ece46261eab718abf544a3a5f6b98643dd264
MuhammadAnas14 <anasm9877@gmail.com>
2022-05-09 01:31:50 +0500

    payload api edit

--- src/Components/LedControl.js
+++ src/Components/LedControl.js
@@ -1,38 +1,63 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { Container, Image, Button } from "react-bootstrap";
 import "./CSS/ledControl.css";
+import axios from "axios";
 
 const Cam = () => {
-
- 
-
   const onChangeLedOff = () => {
+    setSatusButton(
+      <Button value="1" className="btnn-v" onClick={onChangeLedOn}>
+        On
+      </Button>
+    );
 
-    setSatusButton(<Button value="1" className="btnn-v" onClick={onChangeLedOn}>
-    On
-    </Button>)
+    let data = {
+      payload:0
+    }
 
-      
-
-    
-  }
+    axios.post("http://localhost:5000/payload", data, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+    .then((response) => {
+      console.log(response)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  };
 
   const onChangeLedOn = () => {
+    setSatusButton(
+      <Button value="1" className="btnn-v" onClick={onChangeLedOff}>
+        OFF
+      </Button>
+    );
 
-    setSatusButton(<Button value="1" className="btnn-v" onClick={onChangeLedOff}>
-    OFF
-    </Button>)
+    let data = {
+      payload:1
+    } 
 
+    axios.post("http://localhost:5000/payload", data, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+    .then((response) => {
+      console.log(response)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  };
 
-  }
-
-
-  const [ledStatus,setLedStatus] = useState("On")
-  const [StatusButton,setSatusButton] = useState(<Button value="1" className="btnn-v" onClick={onChangeLedOn}>
-  On
-  </Button>)
-
-  
+  const [ledStatus, setLedStatus] = useState("On");
+  const [StatusButton, setSatusButton] = useState(
+    <Button value="1" className="btnn-v" onClick={onChangeLedOn}>
+      On
+    </Button>
+  );
 
   return (
     <Container>
